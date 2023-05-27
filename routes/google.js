d9a2fc62a1f5b7fd19fe23480c75fc4a1fea96d1
tpheejun <70894664+tpheejun@users.noreply.github.com>
2023-05-27 16:45:59 +0900

    update api

--- routes/google.js
+++ routes/google.js
@@ -1,9 +1,32 @@
 var express = require("express");
 var router = express.Router();
+const axios = require("axios");
+const JSONNormalize = require("json-normalize");
 
 /* GET users listing. */
 router.get("/", function (req, res, next) {
-  res.render("google", { title: "Express" });
+  placeId = "ChIJZQkCH3OlfDURoxoozUBZFg8";
+  apikey = "AIzaSyDeqeFz2h2t3SRUE-QiD0Ss4XnniDHZ9_k";
+  axios
+    .get(
+      "https://maps.googleapis.com/maps/api/place/details/json?place_id=" +
+        placeId +
+        "&key=" +
+        apikey
+    )
+    .then((response) => {
+      const placeDetails = response.data;
+      const reviews = placeDetails.result.reviews;
+      console.log(reviews);
+      res.render("google/google", {
+        review: reviews,
+      });
+    });
+
+  // res.render("google/google", {
+  //   title: "Google Map",
+  //   review: review,
+  // });
 });
 
 module.exports = router;
