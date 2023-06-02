89a098dcac7f1bed7aa808a0eb9def8609d39f80
tpheejun <70894664+tpheejun@users.noreply.github.com>
2023-06-02 14:56:36 +0900

    update index and functions

--- public/assets/google/js/index.js
+++ public/assets/google/js/index.js
@@ -1,22 +1,35 @@
+// 마커 관리를 위한 array
 var markers = [];
 
+// 구글맵 생성
 function initMap() {
-  const myLatlng = iniData;
+  // ejs 에 명시된 값 가져오기
+  const myLatlng = LatlngData;
+
   const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 17,
     center: myLatlng,
   });
 
+  // lat, lng 값을 기반으로 마커 만들기
   placeMarker(myLatlng, map);
-  var initialPlace = JSON.stringify(myLatlng);
-  sendData(initialPlace);
+  // console.log(myLatlng);
 
+  // map SDK 의 click 이벤트
   map.addListener("click", (mapsMouseEvent) => {
+    //현재 존재하는 마커 우선 삭제
     deleteMarkers();
-    var clickData = JSON.stringify(mapsMouseEvent.latLng.toJSON(), null, 2);
-    console.log(clickData);
-    sendData(clickData);
 
+    // map 클릭 이벤트에서 응답되는 lat, lng 값
+    var clickData = JSON.stringify(mapsMouseEvent.latLng.toJSON(), null, 2);
+    var clickDataObject = JSON.parse(clickData);
+
+    // console.log(clickDataObject);
+    // console.log(clickDataObject.lat);
+
+    redirectToNewPage(clickDataObject);
+
+    sendData(clickData);
     placeMarker(mapsMouseEvent.latLng, map);
   });
 
@@ -30,21 +43,25 @@ function initMap() {
   }
 }
 
+// 현재 마커를 지우기 위한 사전 작업
 function setMapOnAll(map) {
   for (var i = 0; i < markers.length; i++) {
     markers[i].setMap(map);
   }
 }
 
+// 마커를 지도에서 숨기기
 function clearMarkers() {
   setMapOnAll(null);
 }
 
+// 마커를 삭제하기
 function deleteMarkers() {
   clearMarkers();
   markers = [];
 }
 
+// 서버로 데이터 전달하는 함수
 function sendData(data) {
   fetch("/google/clickdata", {
     method: "POST",
@@ -53,6 +70,10 @@ function sendData(data) {
     },
     body: data,
   });
+}
+
+function redirectToNewPage(data) {
+  window.location.href = `/google/result?lat=${data.lat}&lng=${data.lng}`;
 }
 
 window.initMap = initMap;
--- routes/google.js
+++ routes/google.js
@@ -1,14 +1,11 @@
 var express = require("express");
 var router = express.Router();
 const axios = require("axios");
-global.geoData;
 
-router.post("/clickdata", function (req, res, next) {
-  const data = req.body;
-  global.geoData = data;
-});
+router.get(`/`, function (req, res, next) {
+  let lat = parseFloat(req.query.lat);
+  let lng = parseFloat(req.query.lng);
 
-router.get("/", function (req, res, next) {
   placeId = "ChIJZQkCH3OlfDURoxoozUBZFg8";
   apikey = "AIzaSyDeqeFz2h2t3SRUE-QiD0Ss4XnniDHZ9_k";
   axios
@@ -22,11 +19,26 @@ router.get("/", function (req, res, next) {
       const placeDetails = response.data;
       const reviews = placeDetails.result.reviews;
       // console.log(reviews);
-      res.render("google/google", {
+      res.render(`google/google`, {
         review: reviews,
-        initialLatlng: { lat: 37.49969962500532, lng: 127.03235864639282 },
+        Latlng: { lat: lat, lng: lng },
       });
     });
+});
+
+router.get("/result", function (req, res, next) {
+  let lat = parseFloat(req.query.lat);
+  let lng = parseFloat(req.query.lng);
+
+  res.render("google/result", {
+    Latlng: { lat: lat, lng: lng },
+  });
+});
+
+router.post("/clickdata", function (req, res, next) {
+  const data = req.body;
+  // console.log(data.lat);
+  // return res.redirect(`/google/result?lat=${data.lat}&lng=${data.lng}`);
 });
 
 module.exports = router;
